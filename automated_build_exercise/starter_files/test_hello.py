5abc1bac364afcb71cdd1821b9804bb7cfdf53b3
duypq154 <duypq154@gmail.com>
2023-06-02 18:18:48 +0700

    update hello and test_hello files

--- automated_build_exercise/starter_files/test_hello.py
+++ automated_build_exercise/starter_files/test_hello.py
@@ -1,11 +1,5 @@
 from click.testing import CliRunner
-from hello import hello, toyou, add, subtract
-
-def test_hello():
-    runner = CliRunner()
-    result = runner.invoke(hello, ["--name", "Thor",
-        "--color", "blue"])
-    assert "Thor" in result.output
+from hello import toyou, add, subtract
 
 def setup_function(function):
     print("Running Setup: %s" % function.__name__)
@@ -15,7 +9,6 @@ def setup_function(function):
 def teardown_function(function):
     print("Running Teardown: %s" % function.__name__)
     del function.x
-
 
 ### Run to see failed test
 #def test_hello_add():
